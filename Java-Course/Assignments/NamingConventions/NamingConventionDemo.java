fd6fb11bb6bd1a0e5e31703cb1a713a33b2ba92d
Anuj Kesarwani <anuj.kesarwani@zemosolabs.com>
2023-05-05 12:13:15 +0530

    Modified NamingCoventionDemo

--- Java-Course/Assignments/NamingConventions/NamingConventionDemo.java
+++ Java-Course/Assignments/NamingConventions/NamingConventionDemo.java
@@ -4,9 +4,8 @@ import java.time.LocalDateTime;
 // variable and method - Naming should be camelCase, meaning the first letter of the first word should be lowercase, first letter of each subsequent word should be capitalized (marks, show() getData(), etc.)
 // constant - All letter should be capital (PIE, BRAND, etc.)
 
-class Demo {
-
-    public String getMessage() {
+public class NamingConventionDemo {
+    public static String getMessage() {
         StringBuilder str = new StringBuilder();
         str.append("Hello, ");
         str.append("Everyone!");
@@ -14,21 +13,17 @@ class Demo {
         return message;
     }
 
-    public void getData() {
+    public static void getData() {
         LocalDateTime now = LocalDateTime.now();
         int year = now.getYear();
         int month = now.getMonthValue();
         int day = now.getDayOfMonth();
         System.out.println("Today is "+day+"-"+month+"-"+year);
     }
-}
-
-public class NamingConventionDemo {
 
     public static void main(String[] args) {
-        Demo demo = new Demo();
-        System.out.println(demo.getMessage());
-        demo.getData();
+        System.out.println(getMessage());
+        getData();
     }
 }
 
